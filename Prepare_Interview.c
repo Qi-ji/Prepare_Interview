ebd03a8e61c54259b39414252c0d1098a1784eb3
Qi-ji <874121667@qq.com>
2019-03-13 11:11:57 +0800

    two:get duplication without change the array

--- Prepare_Interview.c
+++ Prepare_Interview.c
@@ -27,3 +27,77 @@ int Duplicate(int number[], int length, int *duplication)
 	}
 	return -1;
 }
+
+题目二
+不修改数组找到重复的数字
+在一个长度为n+1 的数组里，里面所有的数字都在1~n 之间，所有数组中至少有一个数字是重复的，请找出数组中
+任一重复的数字。
+思路：若无重复数字，则1~n 一共有n个数字，而现在有n+1 个数字，说明有重复，
+可以按照二分法来进行查找哪个区间内多了数字。需要运算时间。
+或者是可以构建一个“辅助数组”，需要空间
+注意：找出所有重复？找出任意重复？时间优先？空间优先？
+static int CountRange(const int *aNumber, int iLength, int iStart, int iEnd)
+{
+	int i;
+	int iCount = 0;
+	for (i=0; i<iLength; i++)
+	{
+		if (iStart <= aNumber[i] <= iEnd)
+			iCount++;
+	}
+	return iCount;
+}
+int GetDuplication(const int *aNumber, int iLength) 
+{
+	if (NULL == aNumber || iLength == 0)
+		return -1;
+	int iStart, iEnd, iMiddle;
+	int iCount;
+	
+	iStart = 1;
+	iEnd = iLength - 1;
+	while (iEnd >= iStart)
+	{
+		iMiddle = (iEnd - iStart) >> 1 + iStart;
+	
+		iCount = CountRange(aNumber, iLength, iStart, iMiddle);		/*计算两段直接数字的个数*/
+		if (iEnd == iStart)
+		{
+			if (iCount > 1)
+				return iStart;
+			
+			else
+				break;
+		}
+		
+		
+		if (iCount > (iMiddle - iStart + 1))
+		{
+			iEnd = iMiddle;	
+		}
+		else 
+			iStart = iMiddle + 1;	/*前半部分相等时，去检测后半部分*/				
+	}
+	
+	return -1;
+	
+}
+
+
+
+题目三：二维数组中查找
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
